570aef214aa4fa2ad5cbcc63f6089fb37cb729e1
Kiara <33493552+Kbarias@users.noreply.github.com>
2018-09-21 14:29:36 -0400

    Update Song.cpp

--- Song.cpp
+++ Song.cpp
@@ -8,6 +8,13 @@ Song::Song()
 
 }
 
+Song::Song(const string& title, const string& author, const string& album)
+{
+	title_ = title;
+	author_ = author;
+	album_ = album;
+}
+
 void Song::setTitle(string title) 
 {
 	title_ = title;
@@ -22,17 +29,26 @@ void Song::setAlbum(string album)
 }
 
 
-string Song::getTitle()
+string Song::getTitle() const
 {
 	return title_;
 }
 
-string Song::getAuthor()
+string Song::getAuthor() const
 {
 	return author_;
 }
 
-string Song::getAlbum()
+string Song::getAlbum() const
 {
 	return album_;
+}
+
+bool operator==(const Song& lhs, const Song& rhs)
+{
+	if((lhs.title_ == rhs.title_) && (lhs.author_ == rhs.author_) && (lhs.album_ == rhs.album_))
+	{
+		return true;
+	}
+		return false;
 }
