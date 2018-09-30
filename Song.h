a39f52be86f79b0450380cffc500a48e3ad7838c
Kiara <33493552+Kbarias@users.noreply.github.com>
2018-09-30 14:35:06 -0400

    Update Song.h

--- Song.h
+++ Song.h
@@ -1,39 +1,64 @@
-#ifndef SONG_H_
-#define SONG_H_
-#include <vector>
+/*
+Song.h
+Kiara Barias
+September 25, 2018
+Project 2
+Interface of a song
+*/
+#ifndef SONG_H
+#define SONG_H
 #include <iostream>
+#include <string>
 
-using namespace std;
-
-class Song
-
+class Song 
 {
-
 public:
-	Song();
+  // default constructor
+  Song();
 
-	Song(const string& title, const string& author = "", const string& album = "");
+  //constructor with parameters that sets private members (title, author , and album) to empty
+  Song(const std::string& title, const std::string& author = "", const std::string& album = "");
 
-	void setTitle(string title); 
-	void setAuthor(string author);
-	void setAlbum(string album);
+  /*Task: set the song's title to entered title.  
+  Input:  user entered title.  
+  Output:  title is set to entered title */
+  void setTitle(std::string title);
 
-	string getTitle() const;
+  /*Task: set the song's author to entered author.  
+  Input:  user entered author.  
+  Output:  author is set to entered author */
+  void setAuthor(std::string author);
 
-	string getAuthor() const;
+  /*Task: set the song's album to entered album.  
+  Input:  user entered album.  
+  Output:  album is set to entered album */
+  void setAlbum(std::string album);
 
-	string getAlbum() const;
+  /*Task: return the song's title.  
+  Input:  None.  
+  Output:  return the song's title */
+  std::string getTitle() const;
 
-	friend bool operator==(const Song& lhs, const Song& rhs);
+  /*Task: return the song's author.  
+  Input:  None.  
+  Output:  return the song's author */
+  std::string getAuthor() const;
 
-private: 
+  /*Task: return the song's album.  
+  Input:  None.  
+  Output:  return the song's album */
+  std::string getAlbum() const;
 
-	string title_;
+  /*Task: make sure that we are not repeating the songs by checking that the song's elements are not the same
+  Input:  takes left hand side song and right hand side song  
+  Output: true or false if the songs are the same*/
+  friend bool operator == (const Song& lhs, const Song& rhs);
 
-	string author_;
+private:
+  std::string title_;
+  std::string author_;
+  std::string album_;
 
-	string album_;
+};
 
-}; // end Set
-
-#endif /* SET_H_ */
+#endif // SONG_H
