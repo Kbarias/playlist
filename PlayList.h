e76c015caf207e107b5bf1719540c2998a1f3e13
Kiara <33493552+Kbarias@users.noreply.github.com>
2018-09-30 14:37:08 -0400

    Update PlayList.h

--- PlayList.h
+++ PlayList.h
@@ -1,35 +1,61 @@
-#ifndef PLAYLIST_H_
-#define PLAYLIST_H_
-#include <vector>
-#include "Set.h"
+/*
+PlayList.h
+Kiara Barias
+September 25, 2018
+Project 2
+Interface of a playlist
+*/
+#ifndef PLAY_LIST_H
+#define PLAY_LIST_H
+#include <string>
+#include <iostream>
 #include "Song.h"
+#include "Set.h"
 
-using namespace std;
-
-class PlayList
-{
-
+class PlayList {
 public:
-	PlayList();
+  //default constructor
+  PlayList();
 
-	PlayList(const Song& a_song);
+  /*Task: adds a song into playlist
+  Input:  a song
+  Output: song is put into the playlist */
+  PlayList(const Song& a_song);
 
-	int getNumberOfSongs() const;
+  /*Task: gets the current size of the playlist
+  Input:  none
+  Output: reports the current number of songs in playlist */
+  int getNumberOfSongs() const;
 
-	bool isEmpty() const;
+  /*Task: checks if the playlist is empty
+  Input:  none
+  Output: reports true or false if there are no items in the playlist */
+  bool isEmpty() const;
 
-	bool addSong(const Song& new_song);
+  /*Task: adds a new song to the playlist
+  Input:  new song
+  Output: reports true or false if the new song was succesfully added to the playlist */
+  bool addSong(const Song& new_song);
 
-	bool removeSong(const Song& a_song);
+  /*Task: removes a song from the playlist
+  Input:  a song
+  Output: reports true or false if a song was succesfully removed from the playlist */
+  bool removeSong(const Song& a_song);
 
-	void clearPlayList();
+  /*Task: clears the playlist
+  Input:  none
+  Output: clears the playlist */
+  void clearPlayList();
 
-	void displayPlayList() const;
+  /*Task: prints out the contents of the playlist
+  Input:  none
+  Output: prints out the title, author, and album of every song in the playlist */
+  void displayPlayList() const;
 
-private: 
+private:
+  // declaring a private variable; playlist_ is an object from class set
+  Set<Song> playlist_;
+  
+};
 
-	Set<Song> playlist_; //using the template SET, we're going to plug in Song as the data type and call it "playlist"
-
-}; // end Set
-
-#endif /* PlayList_H_ */
+#endif // PLAY_LIST_H
